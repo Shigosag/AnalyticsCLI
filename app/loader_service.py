be544962811028ee57c4a8375a62c52c3e10c0a0
Shigosag (Segun Arulogun Gabriel) <88502810+Shigosag@users.noreply.github.com>
2026-05-30 13:36:26 +0000

    Update loader_service.py

--- app/loader_service.py
+++ app/loader_service.py
@@ -1,16 +1,17 @@
 import pandas as pd
+import polars as pl
 from app.validators import validate_file
 
 class LoaderService:
 
     @staticmethod
-    def load_data(path: str):
+    def load_data(path: str, use_polars: bool = False):
         validate_file(path)
         if path.endswith(".csv"):
-            return pd.read_csv(path)
+            return pl.read_csv(path) if use_polars else pd.read_csv(path)
         elif path.endswith(".xlsx"):
-            return pd.read_excel(path)
+            return pl.read_excel(path) if use_polars else pd.read_excel(path)
         elif path.endswith(".json"):
-            return pd.read_json(path)
+            return pl.read_json(path) if use_polars else pd.read_json(path)
         else:
             raise ValueError("Unsupported format")
